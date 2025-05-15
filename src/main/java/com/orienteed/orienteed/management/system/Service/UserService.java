7a6e5040dddbefc3ac325efc37d5d1e96f5dd465
omar <omar.alratrot@yahoo.com>
2025-05-15 17:01:19 +0300

    setting things up to implement JWT

--- src/main/java/com/orienteed/orienteed/management/system/Service/UserService.java
+++ src/main/java/com/orienteed/orienteed/management/system/Service/UserService.java
@@ -3,6 +3,7 @@ package com.orienteed.orienteed.management.system.Service;
 import com.orienteed.orienteed.management.system.Model.User;
 import com.orienteed.orienteed.management.system.Repository.userRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,12 +13,14 @@ public class UserService {
     @Autowired
     userRepo userrepo;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
     public List<User> login() {
 
         return userrepo.findAll();
     }
 
     public void addUser(User user) {
+        user.setUserPassword(encoder.encode(user.getUserPassword()));
         userrepo.save(user);
     }
 
